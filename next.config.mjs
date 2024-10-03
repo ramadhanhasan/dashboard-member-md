393cf2b8ffd47e3b538d8acc4757bb667fab8296
ramadhanhasan <ramadhan@femaledaily.com>
2024-10-03 15:21:27 +0700

    feat: add domain md in assets

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['dev-joar.oss-ap-southeast-1.aliyuncs.com', 'via.placeholder.com', 'placehold.co']
+        domains: ['dev-joar.oss-ap-southeast-1.aliyuncs.com', 'via.placeholder.com', 'placehold.co', 'mahir-digital-id.oss-ap-southeast-5.aliyuncs.com']
     }
 };
 
