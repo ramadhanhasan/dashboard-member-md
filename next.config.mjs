ba3740847261d93ecda745879865402ec785dc5c
ramadhanhasan <ramadhan@femaledaily.com>
2024-10-06 04:11:22 +0700

    fix: not automatically login

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    generateEtags: false,
     images: {
         domains: ['dev-joar.oss-ap-southeast-1.aliyuncs.com', 'via.placeholder.com', 'placehold.co', 'mahir-digital-id.oss-ap-southeast-5.aliyuncs.com']
     }
