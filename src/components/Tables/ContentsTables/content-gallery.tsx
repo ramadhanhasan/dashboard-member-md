af458be9a43cefb4699a781183769bd653c53b4f
ramadhanhasan <ramadhan@femaledaily.com>
2024-10-16 01:04:40 +0700

    fix: key in map content

--- src/components/Tables/ContentsTables/content-gallery.tsx
+++ src/components/Tables/ContentsTables/content-gallery.tsx
@@ -181,7 +181,7 @@ export function ContentGallery<TData, TValue>({
           )}
           <div className="mb-5 columns-2 gap-2 md:columns-3 xl:columns-5">
             {data.map((tool: ITools) => (
-              <div className="relative mb-2 break-inside-avoid">
+              <div key={tool.id} className="relative mb-2 break-inside-avoid">
                 <div className="absolute left-0 top-0 w-full">
                   <span className="rounded-r-md bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300">
                     {tool.format}
