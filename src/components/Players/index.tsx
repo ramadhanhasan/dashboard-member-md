12de81c075a243f45e51a3b5490cb8b60a144555
ramadhanhasan <ramadhan@femaledaily.com>
2024-10-19 08:42:24 +0700

    feat: add conditional for gdrivelink in player

--- src/components/Players/index.tsx
+++ src/components/Players/index.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-import LiteYouTubeEmbed from 'react-lite-youtube-embed';
-import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 // const Player = dynamic(() => import("@vime/react").then((mod) => mod.Player), {
 //   ssr: false,
@@ -31,29 +31,45 @@ import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 // );
 
 const PlayerComponent = ({
-    videoId
-} : {videoId : string}) => {
-
-    return (
-        <>
-        <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%', background: '#000' }}>
-      <iframe
-        src={'https://drive.google.com/file/d/1mfgNnibtZkBtu-7_2NWs4WglaPNGfo5_/preview'}
-        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-      ></iframe>
-      <div className="overlay-gdrive"></div>
-    </div>x
-        <LiteYouTubeEmbed 
-          id={videoId}
-          title="Mahir Digital"
-          params="rel=0"
-      />
-        </>
-
-    )
-}
+  videoId,
+  gdriveLink,
+}: {
+  videoId: string;
+  gdriveLink?: string;
+}) => {
+  return (
+    <>
+      {gdriveLink ? (
+        <div
+          style={{
+            position: "relative",
+            paddingBottom: "56.25%",
+            height: 0,
+            overflow: "hidden",
+            maxWidth: "100%",
+            background: "#000",
+          }}
+        >
+          <iframe
+            src={gdriveLink}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+            frameBorder="0"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          ></iframe>
+          <div className="overlay-gdrive"></div>
+        </div>
+      ) : (
+        <LiteYouTubeEmbed id={videoId} title="Mahir Digital" params="rel=0" />
+      )}
+    </>
+  );
+};
 
 export default PlayerComponent;
